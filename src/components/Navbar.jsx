32677c3a3302a55148b1aae86824e2baebc52b5d
capmahin <mahinulhuque1996@gmail.com>
2024-09-09 01:23:28 +0600

    add more option

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <header className='w-full py-5 sm:px-10 px-5 flex
     justify-between items-center'>
-      <nav>
+      <nav className='flex w-full screen-max-width'>
         <img src={appleImg} alt='Apple' width={14}
         height={18}/>
 
