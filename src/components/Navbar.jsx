4c513d4a35a7c2bcf52c644bc07f8e3d362ff09f
capmahin <mahinulhuque1996@gmail.com>
2024-09-09 01:33:28 +0600

    add styling in nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,8 @@ const Navbar = () => {
 
         <div className='flex flex-1 justify-center max-sm:hidden'>
           {navLists.map((nav)=>(
-            <div key={nav}>
+            <div key={nav} className='px-5 text-sm cursor-pointer
+            text-gray hover:text-white transition-all'>
               {nav}
             </div>
           ))}
