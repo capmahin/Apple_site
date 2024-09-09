35af0f3c635656f80c1f6e998f419219809aa86d
capmahin <mahinulhuque1996@gmail.com>
2024-09-09 23:20:40 +0600

    working on duration and stagger

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -6,7 +6,9 @@ const Highlights = () => {
 
   useGSAP(()=>{
     gsap.to('#title', {opacity:1, y:0})
-    gsap.to('.link', {opacity:1, y:0})
+    gsap.to('.link', {opacity:1, y:0, duration:1,
+      stagger:0.25
+    })
   },[])
   return (
     <section id="highlights" className="w-screen overflow-hidden 
