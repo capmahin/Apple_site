a5b096dc2bc36198645f1cb61f6bf9da7f3a73c8
capmahin <mahinulhuque1996@gmail.com>
2024-09-09 11:54:03 +0600

    add opacity

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
   useGSAP(()=>{
-   gsap.to()
+   gsap.to('#hero',{opacity:1})
   },[])
 
 
@@ -12,7 +12,7 @@ const Hero = () => {
       <section className="w-full nav-height bg-black
       relative">
          <div className="h-5/6 w-full flex-center flex-col">
-            <p className="hero-title">iPhone 15 Pro</p>
+            <p id="hero" className="hero-title">iPhone 15 Pro</p>
          </div>
       </section>
   )
