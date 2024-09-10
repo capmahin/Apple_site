6b6847cb7d9c0f1a723793898edd3cc4d709ac18
capmahin <mahinulhuque1996@gmail.com>
2024-09-10 14:19:51 +0600

    add another div

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -85,6 +85,14 @@ const VideoCarousel = () => {
         </div>
       ))}
     </div>
+
+    <div className="relative flex-center mt-10">
+
+      <div className="flex-center py-5 px-7 bg-gray-300
+      backdrop-blur rounded-full">
+
+      </div>
+    </div>
     </>
   )
 }
