51668fcc888440120bc94911b5c51ca90b2640b4
capmahin <mahinulhuque1996@gmail.com>
2024-09-11 23:59:20 +0600

    working on gsap in model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,10 +1,17 @@
+import { useGSAP } from "@gsap/react"
 
 
 const Model = () => {
+
+  useGSAP(()=>{
+    
+  })
   return (
     <section className="common-padding">
        <div className="screen-max-width">
-
+          <h1 id="heading" className="section-heading">
+              Take a closer look.
+          </h1>
        </div>
     </section>
   )
