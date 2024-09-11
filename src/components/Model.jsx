6e51c123b5c1e0618d1b96b000efd9a2c1065374
capmahin <mahinulhuque1996@gmail.com>
2024-09-12 01:03:33 +0600

    add another div

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -17,7 +17,10 @@ const Model = () => {
               Take a closer look.
           </h1>
           <div className="flex flex-col items-center mt-5">
+              <div className="w-full h-[75vh] md:h-[90vh]
+              overflow-hidden relative">
 
+              </div>
           </div>
        </div>
     </section>
