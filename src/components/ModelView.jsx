26f656fafb13495256d6122669b5ff5a856ef164
capmahin <mahinulhuque1996@gmail.com>
2024-09-13 18:38:00 +0600

    remove borde red

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -11,7 +11,7 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotaionSize
     <View
     index={index}
     id={gsapType}
-    className={`border-2 border-red-500 w-full h-full
+    className={` w-full h-full
       ${index === 2} ? 'right-[-100%] : ''`}
     >
          {/* Ambient Light */}
