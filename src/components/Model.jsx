ec18a7174e2a2e293221d927bfe4f81b8fb75c9e
capmahin <mahinulhuque1996@gmail.com>
2024-09-14 01:46:01 +0600

    add another

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -36,11 +36,19 @@ const Model = () => {
   useEffect(()=>{
     if(size === 'large'){
         animateWithGsapTimeline(tl,small,smallRotation,
-          '#view1','#view2'
+          '#view1','#view2',{
+            transform:'translateX(-100%)',
+            duration:2
+          }
         )
     }
     if(size === 'small'){
-
+      animateWithGsapTimeline(tl,large,largeRotation,
+        '#view2','#view1',{
+          transform:'translateX(0)',
+          duration:2
+        }
+      )
     }
   },[size])
 
