2929936d2207b11a42fb3dcf0ac5927a81f0d953
capmahin <mahinulhuque1996@gmail.com>
2024-09-13 20:22:23 +0600

    add'

--- src/components/IPhone.jsx
+++ src/components/IPhone.jsx
@@ -6,14 +6,21 @@ Source: https://sketchfab.com/3d-models/apple-iphone-15-pro-max-black-df17520841
 Title: Apple iPhone 15 Pro Max Black
 */
 
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
 
 function Model(props) {
   const { nodes, materials } = useGLTF('/models/scene.glb')
 
 
-  const texture = useTexture(props.item.img)
+  const texture = useTexture(props.item.img);
+
+  useEffect(()=>{
+    Object.entries(materials).map((material)=>{
+      
+    })
+  },[materials,props,item])
+
 
   return (
     <group {...props} dispose={null}>
