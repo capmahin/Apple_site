aefa17c87e3ce6420208344b819b91f3329cb40b
capmahin <mahinulhuque1996@gmail.com>
2024-09-13 12:30:05 +0600

    add

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -8,7 +8,8 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotaionSize
     <View
     index={index}
     id={gsapType}
-    className="border-2 border-red-500 w-full h-full"
+    className={`border-2 border-red-500 w-full h-full
+      ${index === 2} ? 'right-[-100%] : ''`}
     >
 
     </View>
