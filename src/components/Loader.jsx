b673f0d22b0261883d09062015049ebf976be500
capmahin <mahinulhuque1996@gmail.com>
2024-09-13 20:43:11 +0600

    add another div

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,7 +6,10 @@ const Loader = () => {
     <Html>
         <div className='absolute top-0 left-0 w-full
         h-full flex justify-center items-center'>
-
+        <div className='w-[10vw] h-[10vw]
+        rounded-full'>
+            Loading...
+        </div>
         </div>
     </Html>
   )
