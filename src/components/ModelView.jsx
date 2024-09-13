dd375257f94b9868706719752ae60b3bff77070c
capmahin <mahinulhuque1996@gmail.com>
2024-09-13 20:41:02 +0600

    add loader

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -4,6 +4,7 @@ import * as THREE from 'three';
 import Lights from "./Lights"
 import IPhone from "./IPhone"
 import { Suspense } from "react"
+import Loader from "./Loader";
 
 
 const ModelView = ({index, groupRef, gsapType, controlRef, setRotaionSize
@@ -36,7 +37,7 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotaionSize
         
         <group ref={groupRef} name={`${index ===1} ?
         'small' : 'large'` } position={[0,0,0]}>
-        <Suspense fallback={<Html><div>Loading</div></Html>}>
+        <Suspense fallback={<Loader/>}>
             <IPhone
             scale={index === 1 ? [15,15,15] :
               [17,17,17]
