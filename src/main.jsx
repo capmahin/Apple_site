3f4a2c0acd17b7d905dca6a52c974fed4b31cf21
capmahin <mahinulhuque1996@gmail.com>
2024-09-14 13:39:57 +0600

    add sentury

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,23 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://1c8a9b57bcd347dcec12509b562be01d@o4507301066375168.ingest.us.sentry.io/4507950081048576",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+ 
+  tracesSampleRate: 1.0, 
+  
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
