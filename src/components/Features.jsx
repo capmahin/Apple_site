a488be236417d0a4038c4d0ce44820b903933faf
capmahin <mahinulhuque1996@gmail.com>
2024-09-15 02:36:28 +0600

    addddd

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,7 +4,9 @@ const Features = () => {
   return (
    <section className='h-full common-padding bg-zinc
    relative overflow-hidden'>
+     <div className='screen-max-width'>
 
+     </div>
    </section>
   )
 }
