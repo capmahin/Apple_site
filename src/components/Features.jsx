8018ca588ef3da74384c9be29050f32a8239007c
capmahin <mahinulhuque1996@gmail.com>
2024-09-15 02:38:15 +0600

    add

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -6,7 +6,10 @@ const Features = () => {
    relative overflow-hidden'>
      <div className='screen-max-width'>
         <div className='mb-12 w-full'>
-
+          <h1 id='features_title'
+          className='section-heading'>
+             Explore the full story.
+          </h1>
         </div>
      </div>
    </section>
