9026fad0bedd132e1b9d8ea78ad3d64a46995b91
capmahin <mahinulhuque1996@gmail.com>
2024-09-17 02:53:06 +0600

    add p ta

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -62,7 +62,9 @@ const Features = () => {
 
                  <div className='feature-text-container'>
                     <div className='flex-1 flex-center'>
-
+                        <p className='feature-text g_text'>
+                           iPhone 15 Pro is {' '}
+                        </p> 
                     </div>
                  </div>
               </div>
