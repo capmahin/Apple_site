5c1dc09a038cc45f729a8b6c8fe163e0b3133465
capmahin <mahinulhuque1996@gmail.com>
2024-09-18 12:28:51 +0600

    add div and p tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,11 @@ const Footer = () => {
     
     <footer className='py-5 sm:px-10 px-5'>
     <div className='screen-max-width'>
-
+        <div>
+          <p>
+            
+          </p>
+        </div>
     </div>
     </footer>
   )
