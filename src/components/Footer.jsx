5166b3f72e4cf8a4762b79d96a8794cfeb851617
capmahin <mahinulhuque1996@gmail.com>
2024-09-18 13:10:29 +0600

    finish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { footerLinks } from './../constants/index';
 
 const Footer = () => {
   return (
@@ -14,10 +15,29 @@ const Footer = () => {
             or {' '}
             <span className='underline text-blue'>
                other retailer
-            </span>
+            </span>{' '}
+            near you.
+          </p>
+          <p className="font-semibold text-gray text-xs">
+            Or call 000800-040-1966
           </p>
         </div>
-    </div>
+        <div className="bg-neutral-700 my-5 h-[1px] w-full" />
+
+<div className="flex md:flex-row flex-col md:items-center justify-between">
+  <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved.</p>
+  <div className="flex">
+    {footerLinks.map((link, i) => (
+      <p key={link} className="font-semibold text-gray text-xs">
+        {link}{' '}
+        {i !== footerLinks.length - 1 && (
+          <span className="mx-2"> | </span>
+        )}
+      </p>
+    ))}
+  </div>
+</div>
+</div>
     </footer>
   )
 }
